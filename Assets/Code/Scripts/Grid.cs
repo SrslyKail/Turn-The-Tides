de028321ec8dce8b8687604992d52c22c0bccbf9
Corey Buchan <Xouwan021592@Gmail.com>
2025-02-20 13:18:58 -0800

    Draw mesh for entire grid

--- Assets/Code/Scripts/Grid.cs
+++ Assets/Code/Scripts/Grid.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UIElements;
+using System.Runtime.CompilerServices;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class Grid : MonoBehaviour
 {
     public int xSize, ySize;
     private Vector3[] vertices;
+    private Mesh mesh;
 
     private void Awake()
     {
-        Generate();
+        StartCoroutine(Generate());
     }
 
-    private void Generate()
+    private IEnumerator Generate()
     {
+        WaitForSeconds wait = new WaitForSeconds(0f);
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        int pointsPerQuad = 6;
+
         // Initialize vertices array to hold all our elements
         // Adjacent quads can share the same vertext, so we need 1 more vertex
         // than we have tiles in each direction
@@ -21,13 +29,33 @@ public class Grid : MonoBehaviour
 
         // i is the overall index
         // x and y for x/y coordinates of vertices
-        for(int i = 0, y = 0; y <= ySize; y++)
+        for (int i = 0, y = 0; y <= ySize; y++)
         {
             for(int x = 0; x <= xSize; x++, i++)
             {
                 vertices[i] = new Vector3(x, y);
+                yield return wait;
             }
         }
+        mesh.vertices = vertices;
+        
+        // 6 points for each quad
+        int[] triangles = new int[xSize * ySize * pointsPerQuad];
+
+        //We increment vi because theres 1 more vertex than quad per row
+        for(int ti = 0, vi = 0, y = 0; y < ySize; y++, vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += pointsPerQuad, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+            }
+            
+        }
+
+        mesh.triangles = triangles;
     }
 
     //Draw a sphere at each vertice so we can visualize where they are
