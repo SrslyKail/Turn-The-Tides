046dcacac2b4dc475f89af39c2f7f500bbc59c2b
Corey Buchan <Xouwan021592@Gmail.com>
2025-03-28 14:04:56 -0700

    Add functions to allow MapData objects to update onValidate

--- Assets/Code/MapData.cs
+++ Assets/Code/MapData.cs
@@ -5,29 +5,21 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "MapData", menuName = "Scriptable Objects/Tides Map")]
 public class MapData : ScriptableObject
 {
-    private TextAsset _dataFile;
     public GeoGrid GeoData { get; private set; }
-    public readonly int map_size_offset;
-    public readonly int data_row_count;
-    public readonly int data_column_count;
-    public readonly int map_row_count;
-    public readonly int map_column_count;
 
-    public TextAsset DataFile
+    [SerializeField]
+    private TextAsset _dataFile;
+    public  int mapSizeOffset;
+    public  int dataRowCount;
+    public  int dataColumnCount;
+    public  int mapRowCount;
+    public  int mapColumnCount;
+
+    private void OnValidate()
     {
-        get { return _dataFile; }
-        set
+        if(_dataFile != null)
         {
-            TextAsset old = _dataFile;
-            try
-            {
-                _dataFile = value;
-                GeoData = JSONParser.ParseFromString(_dataFile.text);            }
-            catch (Exception e)
-            {
-                Debug.LogException(e);
-                _dataFile = old;
-            }
+            ProcessDataFile();
         }
     }
 
@@ -36,12 +28,18 @@ public class MapData : ScriptableObject
         int map_size_offset
         )
     {
-        DataFile = dataFile;
-        this.map_size_offset = map_size_offset;
-        data_row_count = GeoData.data.Count;
-        data_column_count = GeoData.data[0].Count;
-        Debug.Log($"Rows:{data_row_count}, columns:{data_column_count}");
-        map_row_count = data_row_count / this.map_size_offset;
-        map_column_count = data_column_count / this.map_size_offset;
+        _dataFile = dataFile;
+        this.mapSizeOffset = map_size_offset;
+        ProcessDataFile();
+    }
+
+    private void ProcessDataFile()
+    {
+        GeoData = JSONParser.ParseFromString(_dataFile.text);
+        dataRowCount = GeoData.data.Count;
+        dataColumnCount = GeoData.data[0].Count;
+        Debug.Log($"Rows:{dataRowCount}, columns:{dataColumnCount}");
+        mapRowCount = dataRowCount / this.mapSizeOffset;
+        mapColumnCount = dataColumnCount / this.mapSizeOffset;
     }
 }
