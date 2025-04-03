4f22a305dd86e8d49fec97722ad36ed2a3c46d62
GurjeetB <gurjeetbhangoo@yahoo.com>
2025-04-03 13:26:54 -0700

    display details about selected tile

--- Assets/Code/Scripts/UI/TileInfoPanel.cs
+++ Assets/Code/Scripts/UI/TileInfoPanel.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using TMPro;
 using TurnTheTides;
 using UnityEngine;
@@ -5,8 +6,17 @@ using UnityEngine;
 public class TileInfoPanel : MonoBehaviour
 {
     public TextMeshProUGUI tileNameText;
+    public TextMeshProUGUI tileDetailsText;
     public void UpdateTileInfo(HexTile tile)
     {
         tileNameText.text = tile.Terrain.ToString();
+
+        StringBuilder details = new StringBuilder();
+        details.AppendLine($"({tile.latitude}, {tile.longitude})");
+        details.AppendLine($"Elevation: {tile.Elevation}m");
+        details.AppendLine($"Land Use: {tile.landUseLabel}");
+        details.AppendLine($"Pollution: {tile.PollutionValue}");
+
+        tileDetailsText.text = details.ToString();
     }
 }
